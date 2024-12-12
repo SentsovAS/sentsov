75ee9568feff4b020dc5802090712edc43a0d6d1
SentsovAS <fuzy83@mail.ru>
2024-12-12 17:38:27 +0200

    Add files via upload

--- linux/lab/lab03/script backup mysql.sh
+++ linux/lab/lab03/script backup mysql.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-destination="/backup/mysql"
-userDB="repl"
-passwordDB="oTUSlave#2020"
 date=`date +%Y-%m-%d`
 MYSQL="/usr/bin/mysql"
 MYSQLDUMP="/usr/bin/mysqldump"
-DBList="$($MYSQL -u $userDB -p$passwordDB -Bse 'show databases')"
-for dbname in $DBlist;
+
+
+for dbname in `mysql -e'show databases;' | grep -v information_schema | grep -v Database`;
 do
-mysqldump --databases --no-tablespaces --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers -u$userDB -p$passwordDB $dbname | gzip > $destination/$date/$dbname.sql.gz
-done;
+$mysqldump $dbname | /usr/bin/gzip -c > /backup/mysql/$date-$dbname.sql.gz;
+done
